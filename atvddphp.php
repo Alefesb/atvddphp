0f85c4f2ed41033ae797d0f23f73d85876b62b44
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-09-25 22:58:00 -0300

    first commit

---
+++ atvddphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    
+    $g1 = $_GET("p1","v1");
+    $g2 = $_GET("p2", "v2");
+    
+    if ()
+    
+    ?>
+</body>
+</html>
